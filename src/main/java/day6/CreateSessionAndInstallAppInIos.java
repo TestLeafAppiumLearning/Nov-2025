9d3667e17ce404c0921934e907ed625d7ffbdf0d
mac <mac@macs-MacBook-Pro.local>
2025-11-16 12:38:06 +0530

    Day 6 code added

---
+++ src/main/java/day6/CreateSessionAndInstallAppInIos.java
@@ -0,0 +1,14 @@
+package day6;
+
+import org.testng.annotations.Test;
+import wrappers.GenericWrappers;
+
+public class CreateSessionInAndroid extends GenericWrappers {
+
+    @Test
+    public void runCode() {
+        launchIosApp("2CDB9EC8-51BA-42D8-8973-3FBD791771D4","","","");
+        sleep(3000);
+        closeApp();
+    }
+}
